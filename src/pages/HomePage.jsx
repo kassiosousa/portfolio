738f18cf292e7f57a24059c91189cde9ea73858c
kassiosousa <kassiomaximus10@gmail.com>
2025-07-27 15:04:45 -0300

    fix: comment temporaly section projectscard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,14 +24,14 @@ function HomePage() {
           <StatCard value="15" label="Tecnologias" />
         </section>
 
-        <section className="mt-16">
+        {/* <section className="mt-16">
           <h3 className="text-2xl font-semibold text-white mb-6">Projetos</h3>
           <div className="grid md:grid-cols-3 gap-6">
             <ProjectCard />
             <ProjectCard />
             <ProjectCard />
           </div>
-        </section>
+        </section> */}
       </main>
   );
 }
